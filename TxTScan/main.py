272259f7f1fe5bcd670d2ca381831de40f74705e
천성찬 <jsh03339@naver.com>
2025-06-04 09:09:29 +0900

    test7

--- TxTScan/main.py
+++ TxTScan/main.py
@@ -3,7 +3,6 @@ from PyQt5.QtWidgets import QApplication
 from ui.LoginView import LoginView
 
 if __name__ == "__main__":
-    print(1 + 2)
     txtScan = QApplication(sys.argv)
     window = LoginView()
     window.show()
